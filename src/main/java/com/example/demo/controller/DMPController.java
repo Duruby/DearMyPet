8a910f39e4136668d1e918cae8a5f4ebf18008a7
Yujeong Lee <Yujeong12833@gmail.com>
2021-10-20 17:20:26 +0900

    탬플릿 추가
    탬플릿 추가(수정 필요)

--- src/main/java/com/example/demo/controller/DMPController.java
+++ src/main/java/com/example/demo/controller/DMPController.java
@@ -1,22 +1,56 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.service.DMPService;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class DMPController {
+    KakaoAPI kakaoApi = new KakaoAPI();
 
-	@Autowired
-	DMPService fabCarService;
+    @RequestMapping("/login")
+    public ModelAndView login(@RequestParam("code") String code, HttpSession session) {
+        ModelAndView mav = new ModelAndView();
+        // 1.번인증코드 요청 전달
+        String accessToken = kakaoApi.getAccessToken(code);
+        // 2번 인증코드로 토큰 전달
+        HashMap<String, Object> userInfo = kakaoApi.getUserInfo(accessToken);
+        
+        System.out.println("login info :" + userInfo.toString());
 
-	@GetMapping("test")
-	public String test() {
-		System.out.println("test");
-		return "test ok";
-	}
+        if (userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("accessToken", accessToken);
+        }
+        mav.addObject("userId", userInfo.get("email"));
+        mav.setViewName("index");
+        System.out.println(mav.getModel());
+        
+        return mav;
+    }
 
+    @RequestMapping(value= "/logout")
+    public ModelAndView logout(HttpSession session) {
+        ModelAndView mav = new ModelAndView();
 
+        kakaoApi.kakaoLogout((String) session.getAttribute("accessToken"));
+        session.removeAttribute("accessToken");
+        session.removeAttribute("userId");
+        mav.setViewName("index");
+        return mav;
+    }
+    @RequestMapping(value="/kakaounlink")
+    public String unlink(HttpSession session) {
+    	kakaoApi.unlink((String)session.getAttribute("access_token")); 
+   session.invalidate(); 
+   return "redirect:/";
+    }
+
+    
 }
--- src/main/java/com/example/demo/controller/KakaoAPI.java
+++ src/main/java/com/example/demo/controller/KakaoAPI.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -128,5 +129,29 @@ public class KakaoAPI {
             e.printStackTrace();
         }
     }
+    public void unlink(String access_Token) { 
+    	String reqURL = "https://kapi.kakao.com/v1/user/unlink";
+    	 try {
+    	 URL url = new URL(reqURL); 
+    	HttpURLConnection conn = (HttpURLConnection) url.openConnection(); 
+    	conn.setRequestMethod("POST");
+    	 conn.setRequestProperty("Authorization", "Bearer " + access_Token); 
+
+    	int responseCode = conn.getResponseCode(); 
+    	System.out.println("responseCode : " + responseCode);
+
+    	 BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream())); 
+
+    	String result = ""; 
+    	String line = ""; 
+
+    	while ((line = br.readLine()) != null) { 
+    	result += line;
+    	 } 
+    	System.out.println(result);
+    	 } catch (IOException e) {
+    	 e.printStackTrace();
+    	 } 
+    	}
 
 }
